b34a2ddb3b847c7ed440f6148a895b520de7a322
Max McKelvey <maxmckelvey@Maxs-MBP.positronix.local>
2023-07-21 13:24:46 -0700

    testing diff electron config

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 // const prepareRenderer = require('electron-next')
 // prepareRenderer("./", 3000)
 
-const { app, BrowserWindow } = require('electron')
+// const { app, BrowserWindow } = require('electron')
 
-const path = require('path')
+// const path = require('path')
 
-
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
+// const createWindow = () => {
+//     const win = new BrowserWindow({
+//       width: 800,
+//       height: 600,
+//       webPreferences: {
+//         preload: path.join(__dirname, 'preload.js')
+//       }
+//     })
   
-    win.loadURL('http://localhost:3000')
-  }
+//     win.loadURL('http://localhost:3000')
+//   }
 
-// createWindow()
+// // createWindow()
 
-app.whenReady().then(() => {
-    createWindow()
-  })
+// app.whenReady().then(() => {
+//     createWindow()
+//   })
+
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', function() {
+    var mainWindow = new BrowserWindow({
+        show: false,
+    });
+    mainWindow.maximize();
+    mainWindow.loadFile('index.html');
+    mainWindow.show();
+});
