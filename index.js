5e1b8cef99e783419945fd9b074bc0921a8fd48b
Max McKelvey <maxmckelvey@Maxs-MBP.positronix.local>
2023-07-21 13:47:49 -0700

    testing diff electron config3

--- index.js
+++ index.js
@@ -20,18 +20,20 @@
 // // createWindow()
 
 // app.whenReady().then(() => {
-//     createWindow()
-//   })
-
-const { app, BrowserWindow } = require('electron');
-
-console.log("Hello World")
-
-app.on('ready', function() {
+  //     createWindow()
+  //   })
+  
+  const { app, BrowserWindow } = require('electron');
+  
+  console.log("Hello World")
+  
+  app.on('ready', function() {
     var mainWindow = new BrowserWindow({
-        show: false,
+      // show: false,
+      fullscreen: true,
     });
-    mainWindow.maximize();
-    mainWindow.loadFile('index.html');
-    mainWindow.show();
+    // mainWindow.maximize();
+    // mainWindow.loadFile('index.html');
+    win.loadURL('http://localhost:3000')
+    // mainWindow.show();
 });
